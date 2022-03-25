6ffd8da44bb63a28e9978017a9b10251ff94a3db
Juarez Lopes <juarezlopesfjr@gmail.com>
2022-03-25 18:18:57 -0300

    Removed unused code

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -8,8 +8,6 @@ import com.example.plugins.configureSerialization
 import com.example.plugins.configureSession
 import io.ktor.server.application.Application
 
-// https://cloud.mongodb.com/v2/623df61ae8a0190f39f85981#clusters
-// IEPQYL8lrghIma52 mongodb password
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
